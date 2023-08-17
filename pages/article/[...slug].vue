49798c10ef33b55f0cfdb1a85f78d0e0c1c01858
fares <djefaflia.fares@gmail.com>
2023-08-17 19:59:24 +0100

    add page about us

---
+++ pages/article/[...slug].vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="max-w-3xl m-auto p-10 lg:mt-10 text-basic prose pink">
+    <h1 class="font-semibold text-4xl mb-6">{{ data.title }}</h1>
+    <ContentDoc  />
+  </section>
+</template>
+
+<script setup>
+  const { path } = useRoute()
+
+  const { data } = await useAsyncData(`content-${path}`, () => {
+    return queryContent().where({ _path: path }).findOne()
+  })
+</script>
+
+<style scoped>
+
+</style>
